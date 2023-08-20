eac27c842200d0abeafdde20010d474b660b2f2d
Jiwon <winning1993@naver.com>
2023-08-20 10:17:07 +0900

    #6.7 Stateful Navigation part Two

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -14,13 +14,7 @@ class MainNavigationScreen extends StatefulWidget {
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
 
-  final screens = [
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-    Container(),
-    StfScreen(key: GlobalKey()),
-    StfScreen(key: GlobalKey()),
-  ];
+  final screens = [];
 
   void _onTap(int index) {
     setState(() {
@@ -30,8 +24,14 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
+    print('im built!');
     return Scaffold(
-      body: screens.elementAt(_selectedIndex),
+      body: Stack(children: [
+        Offstage(offstage: _selectedIndex != 0, child: const StfScreen()),
+        Offstage(offstage: _selectedIndex != 1, child: const StfScreen()),
+        Offstage(offstage: _selectedIndex != 3, child: const StfScreen()),
+        Offstage(offstage: _selectedIndex != 4, child: const StfScreen())
+      ]),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
         child: Padding(
