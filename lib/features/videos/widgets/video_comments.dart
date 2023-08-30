ea7693036404281e0edc8055467cd7553b52dc1c
Jiwon <winning1993@naver.com>
2023-08-31 08:09:03 +0900

    #8.1 Comments

--- lib/features/videos/widgets/video_comments.dart
+++ lib/features/videos/widgets/video_comments.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class VideoComments extends StatefulWidget {
@@ -32,10 +33,60 @@ class _VideoCommentsState extends State<VideoComments> {
                 icon: const FaIcon(FontAwesomeIcons.xmark))
           ],
         ),
-        body: ListView.builder(
+        body: ListView.separated(
+            padding: const EdgeInsets.symmetric(
+                vertical: Sizes.size10, horizontal: Sizes.size16),
+            separatorBuilder: (context, index) => Gaps.v20,
             itemCount: 10,
-            itemBuilder: (context, index) =>
-                Container(child: const Text('im a comment'))),
+            itemBuilder: (context, index) => Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      radius: 18,
+                      child: Text("니꼬"),
+                    ),
+                    Gaps.h10,
+                    Expanded(
+                        child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("니꼬",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: Sizes.size14,
+                                color: Colors.grey.shade500)),
+                        Gaps.v3,
+                        const Text(
+                            "That's not it i've seen the same thing but also in a cave")
+                      ],
+                    )),
+                    Gaps.h10,
+                    Column(
+                      children: [
+                        FaIcon(FontAwesomeIcons.heart,
+                            size: Sizes.size28, color: Colors.grey.shade500),
+                        Gaps.v2,
+                        const Text(
+                          '52.2K',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ],
+                )),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white,
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.grey.shade500,
+                foregroundColor: Colors.white,
+                child: const Text("니꼬"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
