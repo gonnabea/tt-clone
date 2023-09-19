481ae3cd0eaf2af70e2fe91b8d47643ea46ebaf6
Jiwon <winning1993@naver.com>
2023-09-19 09:14:21 +0900

    #11.1 Direct Messages

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -74,6 +74,7 @@ class _ActivityScreenState extends State<ActivityScreen>
         title: GestureDetector(
           onTap: _onTitleTap,
           child: Row(
+            mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const Text("All activity"),
